a6ab423443146dc55fa763bfd00a909c2fdda6ca
Janice Leung <jjw1707@gmail.com>
2013-07-08 11:46:30 -0700

    replaced old client folder with content from dartdoc-viewer repo.

--- client/build.dart
+++ client/build.dart
@@ -1,6 +1,11 @@
+#!/usr/bin/env dart
+
 import 'package:web_ui/component_build.dart';
 import 'dart:io';
+import 'dart:async';
 
 void main() {
-  build(new Options().arguments, ['web/index.html']);
+  var args = new List.from(new Options().arguments);
+  build(args, ['web/index.html'])
+    .then((_) => print('Build finished!'));
 }
